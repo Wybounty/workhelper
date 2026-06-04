208decb48b99f0bef18feab37339c7ee949fb99b
wybounty <siberfrancis971@gmail.com>
2026-06-04 11:50:55 -0400

    First commit

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <main class="min-h-screen w-full bg-[#252525]">
+        <section class="flex items-center justify-center w-full min-h-screen text-white">
+            <h1>Home</h1>
+        </section>
+    </main>
+</template>
